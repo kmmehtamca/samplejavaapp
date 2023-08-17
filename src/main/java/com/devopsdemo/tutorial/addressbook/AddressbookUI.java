2d1ff4a734a4df74039cd84e773a249a5088b608
kmmehtamca <61035067+kmmehtamca@users.noreply.github.com>
2023-08-17 10:04:33 -0500

    Update AddressbookUI.java

--- src/main/java/com/devopsdemo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/devopsdemo/tutorial/addressbook/AddressbookUI.java
@@ -37,8 +37,6 @@ public class AddressbookUI extends UI {
      * vaadin.com/directory.
      */
     TextField filter = new TextField();
-    TextField filterkapil = new TextField();
-    TextField filterkap = new TextField();
     Grid contactList = new Grid();
     Button newContact = new Button("New contact");
 
